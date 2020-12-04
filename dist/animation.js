7acfa9e66960e5fc6927cb332a34debed55cbe8d
rmnaser90 <rmnaser90@gmail.com>
2020-12-04 12:03:02 +0200

    animated entrance with logo

---
+++ dist/animation.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+    const enterLogo = $('#enterLogo')
+    setTimeout(function () {enterLogo.css('display','none')},1000)
+    enterLogo.css('animation-duration','1s')
+    enterLogo.css('animation-name','dissappear')
+    
+},3500)
