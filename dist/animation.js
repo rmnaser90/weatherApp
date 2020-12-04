dd688c47eb35fd6e431344bea4f856b28f1f930c
rmnaser90 <rmnaser90@gmail.com>
2020-12-04 12:53:13 +0200

    animation timing fix

--- dist/animation.js
+++ dist/animation.js
@@ -1,10 +1,9 @@
 setTimeout(function () {
   $('#logoContainer').css('display','block')  
-},520)
+},800)
 setTimeout(function () {
     const enterLogo = $('#enterLogo')
     setTimeout(function () {enterLogo.css('display','none')},1000)
-    enterLogo.css('opacity','1')
     enterLogo.css('animation-duration','1s')
     enterLogo.css('animation-name','dissappear')
     
